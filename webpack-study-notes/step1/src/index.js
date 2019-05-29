849cd06792b882900905a596aee39a0720a7db9f
dirkhe1051931999 <1051931999@qq.com>
2019-05-29 22:07:12 +0800

    [fix] webpack-study-note step1 语法问题

--- webpack-study-notes/step1/src/index.js
+++ webpack-study-notes/step1/src/index.js
@@ -2,10 +2,10 @@ import { text0 } from './text-0.js'
 import { text1 } from './text-1.js'
 
 const textFun = (text1, text0) => {
-  let result = text1 + text0
-  var obj = Objec.assign({}, { result })
+  let result = `${text1} ++ ${text0}`
+  var obj = Object.assign({}, { result })
   let P = document.createElement('p')
-  P.innerHTML = arg.join(' ')
+  P.innerHTML = [1, 2, 3].join(' ')
   var arr = Array.from(new Set([1, 2, 3, 2]))
   var promise = new Promise((resolve, reject) => {
     if (
@@ -13,11 +13,11 @@ const textFun = (text1, text0) => {
         return n > 1
       })
     ) {
-      resolve(123)
+      resolve(obj)
     }
   })
   promise.then(res => {
-    console.log(res)
+    console.log('babel转转一些特殊的语法：Promise/Object.assign/Array.from', res)
   })
   return obj
 }
